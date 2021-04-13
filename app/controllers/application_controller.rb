052b35fc060e12edfb086ace686baf2172f1a695
shotaro sakai <dt.sakaser.hokuoh100@gmail.com>
2021-04-13 11:38:12 +0900

    ユーザー情報をDBへ保存できるように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
 
   private
@@ -9,4 +11,9 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
+  end
+
 end
