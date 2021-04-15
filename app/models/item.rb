608ad8c4099d81a2462a1122d60cfc60dda5e565
shotaro sakai <dt.sakaser.hokuoh100@gmail.com>
2021-04-15 12:42:41 +0900

    バリデーションとアリエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+   validates :product
+   validates :description
+   validates :category_id
+   validates :condition_id
+   validates :ship_base_id
+   validates :ship_date_id
+   validates :prefecture_id
+   validates :price
+   validates :user, foreign_key: true
+  end
+
+
+  has_one :order
+  belongs_to :user
 end
