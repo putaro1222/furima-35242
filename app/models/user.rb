e5766d1273a066f927d3acb1eda05bac587cab5d
shotaro sakai <dt.sakaser.hokuoh100@gmail.com>
2021-04-15 11:26:49 +0900

    バリデーション（with_option）の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,20 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
- with_options presence: true do
-  validates :nickname
-  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
-  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
-  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+
+   with_options format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+  
+   with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
 end
 
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
