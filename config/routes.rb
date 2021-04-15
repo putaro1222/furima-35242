8740b92b1245c2296647f7d0ee825eeada121cae
shotaro sakai <dt.sakaser.hokuoh100@gmail.com>
2021-04-15 12:27:22 +0900

    モデル、マイグレーションの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: "items#index"
-  resources :items, only: :new do
+  resources :items, only: [:index, :new] do
   end
 
 end
