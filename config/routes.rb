e3fe920da621dbd724ac5130b047fa0a6bc894c3
shotaro sakai <dt.sakaser.hokuoh100@gmail.com>
2021-04-19 17:56:44 +0900

    商品詳細表示機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: "items#index"
-  resources :items, only: [:index, :new, :create] do
+  resources :items, only: [:index, :new, :create, :show, :edit] do
   end
 
 end
