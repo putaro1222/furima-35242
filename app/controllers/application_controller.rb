6a8f8fe355f7c66d81497bfc8ad8af35517eea59
shotaro sakai <dt.sakaser.hokuoh100@gmail.com>
2021-04-19 14:18:43 +0900

    Itemコントローラーにauthenticate_user!を追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   private
 
   def basic_auth
@@ -14,5 +13,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
   end
+
 
 end
