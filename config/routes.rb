2a29f508ff5464eb6509762744ccfc5ac9bb3c24
shotaro sakai <dt.sakaser.hokuoh100@gmail.com>
2021-04-19 18:30:57 +0900

    修正依頼の修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show, :edit] do
-  end
+  resources :items, only: [:index, :new, :create, :show]
 
 end
