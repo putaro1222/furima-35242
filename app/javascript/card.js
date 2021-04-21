d4474e71ff48483e813fdcd7d882871243feb3e2
shotaro sakai <dt.sakaser.hokuoh100@gmail.com>
2021-04-21 12:29:02 +0900

    クレジットカード情報のトークン化

---
+++ app/javascript/card.js
@@ -0,0 +1,26 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_7d28098834f970ef2aa41031");
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order_address[number]"),
+      cvc: formData.get("order_address[cvc]"),
+      exp_month: formData.get("order_address[exp_month]"),
+      exp_year: `20${formData.get("order_address[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
