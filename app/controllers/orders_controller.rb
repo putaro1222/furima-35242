3213dcd5bfbe3a48008731be7810977d17c86179
shotaro sakai <dt.sakaser.hokuoh100@gmail.com>
2021-04-22 17:43:23 +0900

    ordersコントローラー　記述まとめ

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,13 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :move_to_index, only: [:index, :create]
+  before_action :set_order, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -37,6 +36,10 @@ class OrdersController < ApplicationController
   def move_to_index
     @item = Item.find(params[:item_id])
     redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+  end
+
+  def set_order
+    @item = Item.find(params[:item_id])
   end
 
 end
