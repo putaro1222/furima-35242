0972617f15626de9bbe5bfc40050476c43e7d16e
shotaro sakai <dt.sakaser.hokuoh100@gmail.com>
2021-04-22 16:53:13 +0900

    move_to_indexの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -31,4 +33,10 @@ class OrdersController < ApplicationController
         currency: 'jpy'
       )
   end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+  end
+
 end
